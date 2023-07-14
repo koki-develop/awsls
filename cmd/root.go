66ae52ab6070cfc98b717c1c02d0d31f5d0531ee
koki-develop <kou.pg.0131@gmail.com>
2023-07-14 15:47:02 +0900

    Add AWS resource tagging and JSON output to root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,52 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws/arn"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "awsls",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+		if err != nil {
+			return err
+		}
+
+		svc := resourcegroupstaggingapi.NewFromConfig(cfg)
+
+		var as []arn.ARN
+
+		params := &resourcegroupstaggingapi.GetResourcesInput{}
+		p := resourcegroupstaggingapi.NewGetResourcesPaginator(svc, params)
+		for p.HasMorePages() {
+			page, err := p.NextPage(context.Background())
+			if err != nil {
+				return err
+			}
+			for _, m := range page.ResourceTagMappingList {
+				a, err := arn.Parse(*m.ResourceARN)
+				if err != nil {
+					return err
+				}
+				as = append(as, a)
+			}
+		}
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		if err := enc.Encode(as); err != nil {
+			return err
+		}
+
+		return nil
+	},
 }
 
 func Execute() {
