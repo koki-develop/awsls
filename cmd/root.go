57a257ac60522dcc523266b7012f7ab6840404b3
koki-develop <kou.pg.0131@gmail.com>
2023-07-15 21:45:31 +0900

    Refactor code to use new AWS and printer packages

--- cmd/root.go
+++ cmd/root.go
@@ -1,47 +1,32 @@
 package cmd
 
 import (
-	"context"
-	"encoding/json"
 	"os"
 
-	"github.com/aws/aws-sdk-go-v2/aws/arn"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/resourcegroupstaggingapi"
+	"github.com/koki-develop/awsls/internal/aws"
+	"github.com/koki-develop/awsls/internal/printer"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "awsls",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg, err := config.LoadDefaultConfig(context.TODO())
+		api, err := aws.New(&aws.Config{})
 		if err != nil {
 			return err
 		}
 
-		svc := resourcegroupstaggingapi.NewFromConfig(cfg)
-
-		var as []arn.ARN
-
-		params := &resourcegroupstaggingapi.GetResourcesInput{}
-		p := resourcegroupstaggingapi.NewGetResourcesPaginator(svc, params)
-		for p.HasMorePages() {
-			page, err := p.NextPage(context.Background())
-			if err != nil {
-				return err
-			}
-			for _, m := range page.ResourceTagMappingList {
-				a, err := arn.Parse(*m.ResourceARN)
-				if err != nil {
-					return err
-				}
-				as = append(as, a)
-			}
+		p, err := printer.New("json")
+		if err != nil {
+			return err
 		}
 
-		enc := json.NewEncoder(os.Stdout)
-		enc.SetIndent("", "  ")
-		if err := enc.Encode(as); err != nil {
+		rs, err := api.GetResources()
+		if err != nil {
+			return err
+		}
+
+		if err := p.Print(os.Stdout, rs); err != nil {
 			return err
 		}
 
